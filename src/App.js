c71b5ec1fa67d6a92585dde66f90e7959e0b3c79
Jagrut-Khetalpar <khetalparjagrut@gmail.com>
2022-09-18 19:48:09 +0530

    Final project
    Final stage of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import { useState } from "react";
+import Alert from "./components/Alert";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light"); // Wether dark mode enable or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(33,37,41)";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils-Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "Text Utils-Light Mode";
+      // setInterval(() => {
+      //   document.title = "Text Utils Running now";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Text Utils - You got a lottery";
+      // }, 1500);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Navbar title="Utils" aboutText="About Text Utils" /> */}
+      {/* <Navbar /> */}
+      {/* <Router> */}
+      <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+
+      <div className="container my-3">
+        {/* <Switch> */}
+        {/* <Route exact path="/about"> */}
+        {/* <About /> */}
+        {/* </Route> */}
+        {/* <Route exact path="/"> */}
+        <TextForm
+          heading="Enter the text to analyze below"
+          mode={mode}
+          showAlert={showAlert}
+        />
+        {/* </Route> */}
+        {/* </Switch> */}
+      </div>
+      {/* </Router> */}
+    </>
   );
 }
 
